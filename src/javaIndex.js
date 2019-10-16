6798fc4bed353b7cdb2a7fb13c72284583de2519
martincito123 <sebastian187@outlook.es>
2019-10-16 17:22:08 -0500

    subiendo trivia

---
+++ src/javaIndex.js
@@ -0,0 +1,18 @@
+var questions = [
+
+"What do you have to consider when you want to create an Etherchannel",
+"what is the correct order related to the OSI model",
+"What is the algorhitm that EIGRP use"
+
+]
+    
+var answers = [
+["same number of vlan and same group of etherchannel on both sides","only bandwidth","the frecuency of both switches","The MAC"],
+["aplicacion ,sesion ,presentacion,transporte,enlace de datos,fisica","layer1-layer5-layer4-layer3","vtp,stp,fstp,fisica","it doesn´t exist"],
+["DUAL (difusing update algorithm)", "dijkstra","Bellman","STP",]
+];
+
+
+
+var indice_ramdom = Math.random(Math.random())*questions.lenght
+
